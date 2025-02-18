194df6a4a0a081af14755e2f61ce337c53d64314
twocucao <twocucao@gmail.com>
2025-02-18 22:52:23 +0800

    format and patch

--- chumpy/__init__.py
+++ chumpy/__init__.py
@@ -1,3 +1,27 @@
+import inspect
+from collections import namedtuple
+
+# 创建一个与旧的 ArgSpec 类似的 namedtuple
+ArgSpec = namedtuple("ArgSpec", ["args", "varargs", "keywords", "defaults"])
+
+
+def my_getargspec(func):
+    """
+    inspect.getargspec 的替代函数，用于 Monkey Patching。
+    这里使用 inspect.getfullargspec 实现。
+    """
+    full_argspec = inspect.getfullargspec(func)
+    return ArgSpec(
+        full_argspec.args,
+        full_argspec.varargs,
+        full_argspec.varkw,
+        full_argspec.defaults,
+    )
+
+
+# 在导入 chumpy 之前应用 Monkey Patch
+inspect.getargspec = my_getargspec
+
 from .ch import *
 from .logic import *
 
@@ -12,15 +36,16 @@ from .version import version as __version__
 def test():
     from os.path import split
     import unittest
-    test_loader= unittest.TestLoader()
+
+    test_loader = unittest.TestLoader()
     test_loader = test_loader.discover(split(__file__)[0])
     test_runner = unittest.TextTestRunner()
-    test_runner.run( test_loader )
+    test_runner.run(test_loader)
 
 
 demos = {}
 
-demos['scalar'] = """
+demos["scalar"] = """
 import chumpy as ch
 
 [x1, x2, x3] = ch.array(10), ch.array(20), ch.array(30)
@@ -29,7 +54,7 @@ print result # prints [ 60.]
 print result.dr_wrt(x1) # prints 1
 """
 
-demos['show_tree'] = """
+demos["show_tree"] = """
 import chumpy as ch
 
 [x1, x2, x3] = ch.array(10), ch.array(20), ch.array(30)
@@ -43,7 +68,7 @@ x3.label='x3' # for clarity in show_tree()
 x2.show_tree(cachelim=1e-4) # in MB
 """
 
-demos['matrix'] = """
+demos["matrix"] = """
 import chumpy as ch
 
 x1, x2, x3, x4 = ch.eye(10), ch.array(1), ch.array(5), ch.array(10)
@@ -52,7 +77,7 @@ print y
 print y.dr_wrt(x2)
 """
 
-demos['linalg'] = """
+demos["linalg"] = """
 import chumpy as ch
 
 m = [ch.random.randn(100).reshape((10,10)) for i in range(3)]
@@ -61,7 +86,7 @@ print y.shape
 print y.dr_wrt(m[0]).shape
 """
 
-demos['inheritance'] = """
+demos["inheritance"] = """
 import chumpy as ch
 import numpy as np
 
@@ -84,7 +109,7 @@ print result.r
 print result.dr_wrt(x1)
 """
 
-demos['optimization'] = """
+demos["optimization"] = """
 import chumpy as ch
 
 x = ch.zeros(10)
@@ -102,16 +127,14 @@ print y # should be all 0.5
 """
 
 
-
-
 def demo(which=None):
     if which not in demos:
-        print('Please indicate which demo you want, as follows:')
+        print("Please indicate which demo you want, as follows:")
         for key in demos:
             print("\tdemo('%s')" % (key,))
         return
 
-    print('- - - - - - - - - - - <CODE> - - - - - - - - - - - -')
+    print("- - - - - - - - - - - <CODE> - - - - - - - - - - - -")
     print(demos[which])
-    print('- - - - - - - - - - - </CODE> - - - - - - - - - - - -\n')
-    exec('global np\n' + demos[which], globals(), locals())
+    print("- - - - - - - - - - - </CODE> - - - - - - - - - - - -\n")
+    exec("global np\n" + demos[which], globals(), locals())
